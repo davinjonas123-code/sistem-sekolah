f875855fc9f0b90eeb50742c154ae82e5ba9102c
davinjonas123-code <davinjonas123@gmail.com>
2026-01-29 13:03:33 +0700

    feat: implement simple router

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,31 +1,33 @@
 <?php
-
+ 
 namespace App\Core;
-class Router
 
-    {
+use App\Controllers\StudentController;
+ 
+class Router
+{
     public function run()
     {
-        $method = $_SERVER['REQUEST_METHOD'];
-        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-    if ($method == 'GET' && $uri == '/students') {
-        echo '<h1>Daftar siswa</h1>';
-    echo '<p>Menampilkan daftar siswa</p>';
-    return;
+      $method = $_SERVER['REQUEST_METHOD'];
+      $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
  
+     
+      if($method == 'GET' && $uri == '/students') {
+         require_once './app/controllers/StudentController.php';
+         $controller = new StudentController();
+         $controller->index();
+        return;
     }
-    if ($method == 'GET' && $uri == '/students/create') {
-        echo '<h1>Tambah siswa</h1>';
-    echo '<p>Menampilkan form tambah siswa</p>';
-    return;
  
+    if($method == 'GET' && $uri == '/students/create') {
+         require_once '.app/controllers/StudentController.php';
+         $controller = new StudentController();
+         $controller->create(); 
+        return;
     }
-
+ 
     http_response_code(404);
     echo '<h1>404 - Page Not Found</h1>';
     }
-    }
-
-
-?>
+}
+   
