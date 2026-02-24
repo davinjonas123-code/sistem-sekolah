f552f646961ce42564a4af49861b73c6bcd362f3
davinjonas123-code <davinjonas123@gmail.com>
2026-02-24 13:18:33 +0700

    wipL: setup tailwind

--- public/index.php
+++ public/index.php
@@ -5,5 +5,10 @@ require_once  './app/core/Router.php';
 use App\Core\Router;
 
 $router = new Router();
+
+$router->add('GET', '/students', 'StudentController', 'index');
+$router->add('GET', '/students/create', 'StudentController', 'create');
+$router->add('GET', '/students/{id}', 'StudentController', 'show');
+
 $router->run();
 ?>
