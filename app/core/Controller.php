f8c1a3de724ee6379a27f3e56bcf3797b6f4fb37
davinjonas123-code <davinjonas123@gmail.com>
2026-04-09 13:11:26 +0700

    feat: improvement view

---
+++ app/core/Controller.php
@@ -0,0 +1,16 @@
+<?php 
+namespace App\Core;
+
+class Controller
+
+{
+    public function view(string $view, array $data = [])
+ {
+    extract($data);
+    $view = str_replace('.', '/', $view);
+    require_once "../app/views/{$view}.php";
+ }
+}
+
+
+?>
