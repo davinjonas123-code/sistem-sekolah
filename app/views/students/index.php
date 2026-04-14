c42305736bdfc574cfa6c720a1fca9fe85b91d74
davinjonas123-code <davinjonas123@gmail.com>
2026-04-14 13:15:55 +0700

    feat: implement student index page b

--- app/views/students/index.php
+++ app/views/students/index.php
@@ -1,4 +1,4 @@
-<div class="mt-8 space-y-2">
+ <div class="mt-8 space-y-2">
             <!-- Card Header Start -->
              <div class="p-4 shadow rounded-lg bg-white">
                 <h1 class="text-2xl font-bold">Daftar Siswa</h1>
@@ -8,7 +8,7 @@
  
              <!-- Card Body Start -->
  
- <div class="bg-white shadow rounded-lg">
+ <div class="bg white shadow rounded-lg">
     <table class="w-full">
     <thead class="bg-gray-200">
         <tr>
@@ -16,18 +16,25 @@
             <th class="px-4 py-2 text-left">Nama</th>
             <th class="px-4 py-2 text-left">NIS</th>
             <th class="px-4 py-2 text-left">Kelas</th>
-            <th class="px-4 py-2 text-left">No Telepon</th>
+            <th class="px-4 py-2 text-left">No telepon</th>
             <th class="px-4 py-2">Aksi</th>
         </tr>
     </thead>
-
+ 
     <tbody>
-        <tr>
-        <td class="px-4 py-2 text-left">1.</td>
-        <td class="px-4 py-2 text-left">Davin</td>
-        <td class="px-4 py-2 text-left">9562</td>     
-        <td class="px-4 py-2 text-left">XI TKJ 2</td>  
-        <td class="px-4 py-2 text-left">08123456789</td>
+        <?php foreach($students as $index => $student): ?>
+            <tr>
+        <td class="px-4 py-2 text-align">
+            <?= $index + 1?>
+        </td>
+        <td class="px-4 py-2 text-align">
+            <?= $student['name']?>
+        </td>
+        <td class="px-4 py-2 text-align">
+            <?= $student['nis']?>
+        </td>    
+        <td class="px-4 py-2 text-align">XI TKJ 2</td>  
+        <td class="px-4 py-2 text-align">08123456789</td>
         <td class="px-4 py-2">
             <div class="flex justify-center items-center gap-4">
                 <a href="/students/1" class="text-green-500">Detail</a>
@@ -35,11 +42,73 @@
                  <a href="" class="text-red-500">Hapus</a>
             </div>
         </td>
-
+ 
         </tr>
+        <?php endforeach ?>
+ 
     </tbody>
     </table>
  </div>
  
              <!-- Card Body End -->
         </div>
+ 
+ <div class="mt-8 space-y-2">
+            <!-- Card Header Start -->
+             <div class="p-4 shadow rounded-lg bg-white">
+                <h1 class="text-2xl font-bold">Daftar Siswa</h1>
+                <p>Menampilkan daftar siswa yang terdaftar</p>
+             </div>
+             <!-- Card Header End -->
+ 
+             <!-- Card Body Start -->
+ 
+ <div class="bg white shadow rounded-lg">
+    <table class="w-full">
+    <thead class="bg-gray-200">
+        <tr>
+            <th class="px-4 py-2 text-left">No</th>
+            <th class="px-4 py-2 text-left">Nama</th>
+            <th class="px-4 py-2 text-left">NIS</th>
+            <th class="px-4 py-2 text-left">Kelas</th>
+            <th class="px-4 py-2 text-left">No telepon</th>
+            <th class="px-4 py-2">Aksi</th>
+        </tr>
+    </thead>
+ 
+    <tbody>
+        <?php foreach($students as $index => $student): ?>
+            <tr>
+        <td class="px-4 py-2 text-align">
+            <?= $index + 1?>
+        </td>
+        <td class="px-4 py-2 text-align">
+            <?= $student['name']?>
+        </td>
+        <td class="px-4 py-2 text-align">
+            <?= $student['nis']?>
+        </td>    
+        <td class="px-4 py-2 text-align">
+            <?= $student['class']?>
+        </td>  
+        <td class="px-4 py-2 text-align">
+            <?= $student['phone_number']?>
+        </td>
+        <td class="px-4 py-2">
+            <div class="flex justify-center items-center gap-4">
+                <a href="/students/1" class="text-green-500">Detail</a>
+                 <a href="/students/1/edit" class="text-yellow-500">Edit</a>
+                 <a href="" class="text-red-500">Hapus</a>
+            </div>
+        </td>
+ 
+        </tr>
+        <?php endforeach ?>
+ 
+    </tbody>
+    </table>
+ </div>
+ 
+             <!-- Card Body End -->
+        </div>
+ 
