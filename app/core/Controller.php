3c5c61b97c67e7b7adebd67639cb746d4f963d28
davinjonas123-code <davinjonas123@gmail.com>
2026-04-14 12:30:31 +0700

    feat: implement database layer

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -8,8 +8,11 @@ class Controller
  {
     extract($data);
     $view = str_replace('.', '/', $view);
-    require_once "../app/views/{$view}.php";
+
+    $content = "../app/views/{$view}.php";
+    require_once "../app/views/layouts/app.php";
  }
+ 
 }
 
 
